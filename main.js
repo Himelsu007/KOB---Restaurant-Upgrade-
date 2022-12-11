1c5f20d70945ad81eecfbf0007d9160759c2ca53
Lucas Galarani <111251426+LucasGalarani@users.noreply.github.com>
2022-12-10 21:08:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-// Carrossel  sendo feito
+<script type="text/javascript">
+function slide1(){
+document.getElementById('id').src="1.bmp";
+setTimeout("slide2()", 3000)
+document.getElementById('aId').href="link1.html"
+}
 
-$('.carrosel').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-});
+function slide2(){
+document.getElementById('id').src="2.bmp";
+setTimeout("slide3()", 3000)
+document.getElementById('aId').href="link2.html"
+}
+
+function slide3(){
+document.getElementById('id').src="3.bmp";
+setTimeout("slide1()", 3000)
+document.getElementById('aId').href="link3.html"
+}
+</script>
+<body onLoad="slide1()">
+<a id="aId"><img id="id"></a>
+</body>
+</html>
